cd0813c7c91614cac8fe2352c7eaa1d3f51efd88
Thorbeorn <52549642+thorbeorn@users.noreply.github.com>
2025-02-12 17:12:04 +0100

    Add files via upload

--- panel/properties.js
+++ panel/properties.js
@@ -1,10 +1,10 @@
-const { error } = require('console');
 const fs = require('fs');
-const cheminFichier = 'server.properties';
+const PropertiesFilePath = 'server.properties';
 
+// Read 
 function getProperties() {
   return new Promise((resolve, reject) => {
-    fs.readFile(cheminFichier, 'utf-8', (err, data) => {
+    fs.readFile(PropertiesFilePath, 'utf-8', (err, data) => {
       if (err) {
         return reject(new Error('Erreur de lecture du fichier: ' + err));
       }
@@ -19,11 +19,63 @@ function getProperties() {
     });
   });
 }
+function getPropertie(key) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(PropertiesFilePath, 'utf-8', (err, data) => {
+      if (err) {
+        return reject(new Error('Erreur de lecture du fichier: ' + err));
+      }
+      const lignes = data.split('\n');
+      try {
+        let propertie = {};
+        if (propertieRaw = lignes.find(ligne => ligne.includes(key))) {
+          const [cle, valeur] = propertieRaw.split('=');
+          propertie[cle.trim()] = valeur ? valeur.trim() : '';
+        }
+        resolve(propertie);
+      } catch (e) {
+        return reject(new Error('Erreur de recheche dans le fichier: ' + e));
+      }
+    });
+  });
+}
 
-getProperties().then(properties => console.log(properties)).catch(error => console.error(error));
-
+// Update
+function updatePropertie(key, value) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(PropertiesFilePath, 'utf-8', (err, data) => {
+      if (err) {
+        return reject(new Error('Erreur de lecture du fichier: ' + err));
+      }
+      const lignes = data.split('\n');
+      let propertieUpdated = false;
+      const updatedLines = lignes.map(ligne => {
+        if (ligne.includes(key)) {
+          propertieUpdated = true;
+          return `${key}=${value}`;
+        }
+        return ligne;
+      });
+      if (!propertieUpdated) {
+        resolve(false);
+      }
+      fs.writeFile(PropertiesFilePath, updatedLines.join('\n'), 'utf-8', (err) => {
+        if (err) {
+          return reject(new Error('Erreur d\'écriture dans le fichier: ' + err));
+        }
+        resolve(true);
+      });
+    });
+  });
+}
 
 // Create = pas besoin le fichier se generer automatiquement
+//==================
 // Read 
+// getProperties().then(properties => console.log(properties)).catch(error => console.error(error));
+// getPropertie("white-list").then(propertie => console.log(propertie)).catch(error => console.error(error));
+//==================
 // Update
+// updatePropertie("white-listd", "false").then(result => console.log(result)).catch(error => console.error(error));
+//==================
 // Delete = pas besoin le fichier se generer automatiquement
